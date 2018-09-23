38a5d53e5821f2fc2131218ff1d8fc5d8aa4432c
Naveen Saggam <thenaveensaggam@gmail.com>
2018-09-23 10:47:15 +0530

    Created New Template

---
+++ AngularJS_Starter_Files/12_Angular_Number_App/12_app.js
@@ -0,0 +1,22 @@
+app.directive('numbersOnly', function() {
+    var directive = {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9-]/g, '');
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+
+    };
+    return directive;
+});
